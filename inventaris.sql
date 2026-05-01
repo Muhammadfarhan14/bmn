e229919400bb37697d03faa084993eb783f1f515
Muhammad Farhan 026 <104129469+Muhammadfarhan14@users.noreply.github.com>
2026-05-01 23:02:14 +0800

    Add files via upload

--- inventaris.sql
+++ inventaris.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 30 Apr 2026 pada 09.03
+-- Waktu pembuatan: 01 Bulan Mei 2026 pada 17.01
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -18,8 +18,59 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pengaduan_imigrasi`
+-- Database: `inventaris`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `nama_barang` varchar(100) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `lokasi` varchar(100) DEFAULT NULL,
+  `kondisi` varchar(50) DEFAULT NULL,
+  `kode_barang` varchar(50) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `kategori` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama_barang`, `jumlah`, `lokasi`, `kondisi`, `kode_barang`, `status`, `kategori`) VALUES
+(1, 'Pulpen', 11, 'Gudang', 'baik', '1', 'Tersedia', 'ATK'),
+(2, 'Botol', 10, 'Gudang', 'baik', '2', 'Tersedia', 'Elektronik'),
+(3, 'Amplop', 12, 'Gudang', 'baik', '3', 'Tersedia', 'Persediaan'),
+(4, 'Selang', 12, 'Gudang', 'baik', '4', 'Tersedia', 'Peralatan'),
+(6, 'Tissue', 12, 'Gudang', 'baik', '5', 'Tersedia', 'Persediaan');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `barang_id` int(11) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id`, `user_id`, `barang_id`, `jumlah`, `status`, `tanggal`) VALUES
+(1, 2, 1, 1, 'disetujui', '2026-05-01 14:20:05'),
+(2, 2, 2, 2, 'disetujui', '2026-05-01 14:24:42');
 
 -- --------------------------------------------------------
 
@@ -29,24 +80,35 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `nama_lengkap` varchar(100) DEFAULT NULL,
-  `last_login` datetime DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `role` enum('admin','user') DEFAULT 'admin'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+  `nama` varchar(100) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `nama_lengkap`, `last_login`, `created_at`, `role`) VALUES
-(1, 'admin', 'imigrasi2025', 'Administrator', '2026-04-30 13:41:41', '2025-12-24 03:16:09', 'admin');
+INSERT INTO `users` (`id`, `nama`, `username`, `password`, `role`) VALUES
+(1, 'Admin', 'admin', '123', 'admin'),
+(2, 'User', 'user', '123', 'user');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `users`
@@ -59,10 +121,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
